fa15a5cf147ca2fbc8ab2eb57eea8588bdc3d5be
Egorprivet <124155851+Egorprivet@users.noreply.github.com>
2025-04-13 20:41:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,47 @@ class GameSprite(sprite.Sprite): #Основной класс спрайта
  
 class Player(GameSprite):
     def update(self):
-        keys_pressed = key.get_pressed()
-        if keys_pressed[K_a] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys_pressed[K_d] and self.rect.x < win_width - 80:
+        keys = key.get_pressed()
+        if keys_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.x < 480:
             self.rect.x += self.speed
+        
+class Player2(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.x < 480:
+            self.rect.x += self.speed
+# window     
+window = display.set_mode((500, 700))
+display.set_caption('ping_pong')
+background = transform.scale(image.load('table.png'), (500, 700))
+
+lost = 0 # Счётчик пропущенных мячей
+score = 0
+
+game = True
+finish = False
+#Таймер
+clock = time.Clock()
+FPS = 60
+
+player = Player('rocket.jpg', 15, 218, 15)
+player2 = Player('rocket.jpg', 620, 217, 15)
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish == False:
+        window.blit(background, (0,0))
+        player.update()
+        player.reset()
+        player2.update()
+        player2.reset()
+        
+    display.update()
+    clock.tick(FPS)
+    
