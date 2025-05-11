f907a1025e0b9ec08f2f6ac4a9a71678ad428f3b
Egorprivet <124155851+Egorprivet@users.noreply.github.com>
2025-05-11 19:53:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -54,10 +54,10 @@ score1 = 0
 score2 = 0
     
 def game_over():
-    if score1 >= 5 and score1 - score2 >= 1:
+    if score1 >= 10 and score1 - score2 >= 1:
         window.blit(lose1, (260, 250))
         window.blit(win2, (260, 190))
-    if score2 >= 5 and score2 - score1 >= 1:
+    if score2 >= 10 and score2 - score1 >= 1:
         window.blit(lose2, (260, 250))
         window.blit(win1, (260, 190))
 
@@ -81,7 +81,6 @@ mixer.init()
 mixer.music.load('mm.ogg')
 mixer.music.play()
 
-
 # Таймер
 clock = time.Clock()
 FPS = 60
@@ -92,6 +91,9 @@ lost2 = 0
 speed_x = 5
 speed_y = 5
 
+
+cooldown = 0
+
 # Создание  объектов
 player = Player('pl1.png', 15, 218, 15)
 player2 = Player2('pl2.png', 620, 217, 15)
@@ -101,18 +103,19 @@ game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+
+
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
     if finish == False:
-        
         window.blit(background, (0,0)) 
         score_1 = font1.render('Player 1 score:'+str(score1), 1, (255, 255, 255))
         score_2 = font1.render('Player 2 score:'+str(score2), 1, (255, 255, 255))
         window.blit(score_1, (27, 27))
         window.blit(score_2,(403, 27))
-        
         player.update()
         player.reset()
         player2.update()
@@ -120,11 +123,17 @@ while game:
         ball.update()
         ball.reset()
         game_over()
-        if sprite.collide_rect(ball, player) or sprite.collide_rect(ball, player2):
+        if sprite.collide_rect(ball, player) and cooldown > 9:
             speed_x *= -1
+            cooldown = 0
+        if sprite.collide_rect(ball, player2) and cooldown > 9:
+            speed_x *= -1
+            cooldown = 0
+    cooldown += 1
         
     display.update()
     clock.tick(60)
+
 
 
 
